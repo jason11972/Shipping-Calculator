e4262625c5a48e7e556d71d1e300749fb546408e
Jason Wilson <jason11972@yahoo.com>
2016-07-28 16:31:39 -0700

    v0.1 - Jason Wilson

--- javascript.js
+++ javascript.js
@@ -1,46 +1,43 @@
 /**
  * Created by Jason Wilson on 7/19/2016.
  */
-var shipping_time = 5;
+// var shipping_time = 5;
 
 $(document).ready(function() {
 
     function apply_event_handlers() {
+    
 
+        $('input').keydown(function (validate_keydown) {
+        
+        });
+    
+        $('radio').on('click', function (change_shipping_type) {
+    
+        });
 
-//         $('input').keydown(function (validate_keydown) {
-
-//         });
-//
-//         $('radio').on('click', function (change_shipping_type) {
-//
-//         });
-
-//         $('calculate').on('click', function (display_shipping) {
-
-//         });
+        $('calculate').on('click', function (display_shipping) {
+        
+        });
     }
-//     $('input').on('keypress', validate_keydown);
-//KEYDOWN FUNCTION IS FULLY FUNCTIONAL BUT CHANGING INPUT TYPE TO "NUMBER" INSTEAD OF "TEXT" MAKES THIS FUNCTION IRRELEVANT//
-//     function validate_keydown(e) {
-//         var charCode = (e.which) ? e.which : e.keyCode;
-//         //basically this says anything that's not 0-9 will return false and not be displayed. decimal point is 46, so if
-//         //decimal point is used it returns true.
-//         if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-//                 //48 thru 57 is the charCode for numbers 0-9 on keyboard, so if its less then n48 or greater then 57 then
-//                 //its not 0-9
-
-//             return false;
-
-//         return true;
-//     }
-
-    // function change_shipping_type() {
-    //     shipping_time = document.getElementById('radio').value;
-    // }
-
+    $('input').on('keypress', validate_keydown);
+    //THIS KEYDOWN FUNCTION ISNT NECESSARY IF YOU JUST CHANGE INPUT "TYPE" IN HTML TO "NUMBER" INSTEAD OF "TEXT"//
+    function validate_keydown(e) {
+        var charCode = (e.which) ? e.which : e.keyCode;
+        //basically this says anything that's not 0-9 will return false and not be displayed. decimal point is 46, so if
+        //decimal point is used it returns true.
+        if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+                //48 thru 57 is the charCode for numbers 0-9 on keyboard, so if its less then 48 or greater then 57 then
+                //its not 0-9
+    
+            return false;
+    
+        return true;
+    }
 
 });
+
+
 function calculate_shipping() {
     var weightInput = document.getElementById("weightinput").value;
     document.getElementById("pounds").innerHTML = weightInput + " lbs";
@@ -65,15 +62,15 @@ function calculate_shipping() {
 
 
 
-function multiplier() {
+function shippingDays() {
     days = parseInt(document.querySelector('input[name="days"]:checked').value);
     if (days === 5) {
-        cost = days * 1;
+        cost = shippingcost * 1;
     }else
     if(days === 3) {
-        cost = days * 1.5;
+        cost = shippingcost * 1.5;
     } else {
-        cost = days * 2;
+        cost = shippingcost * 2;
     }
     var date = new Date();
     var todays_day = date.getDay();
@@ -98,9 +95,9 @@ function multiplier() {
 
 }
 
-function totalshippingcost() {
-    var totalcost = cost + shippingcost;
-    document.getElementById("shippingcost").innerHTML = "<b>Cost: $</b>" + totalcost.toFixed(2);
+function totalShippingCost() {
+    var total_cost = cost + shippingcost;
+    document.getElementById("shippingcost").innerHTML = "<b>Cost: $</b>" + total_cost.toFixed(2);
 
-    console.log("Total cost for package is: $" + totalcost.toFixed(2));
+    console.log("Total cost for package is: $" + total_cost.toFixed(2));
 }
